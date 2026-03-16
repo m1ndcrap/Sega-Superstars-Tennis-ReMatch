34a12c058b579694bdac339be3e714a8dc22d372
Emaad Khan <e4emaad@gmail.com>
2026-03-16 10:32:18 -0400

    new title + icon + updated to rexglue v0.4.2

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// sst_rematch - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+
+#include "generated/sst_rematch_config.h"
+#include "generated/sst_rematch_init.h"
+
+#include "sst_rematch_app.h"
+
+REX_DEFINE_APP(sst_rematch, SstRematchApp::Create)
---
+++ src/sst_rematch_app.h
@@ -0,0 +1,26 @@
+// sst_rematch - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+// Customize your app by overriding virtual hooks from rex::ReXApp.
+
+#pragma once
+
+#include <rex/rex_app.h>
+
+class SstRematchApp : public rex::ReXApp {
+ public:
+  using rex::ReXApp::ReXApp;
+
+  static std::unique_ptr<rex::ui::WindowedApp> Create(
+      rex::ui::WindowedAppContext& ctx) {
+    return std::unique_ptr<SstRematchApp>(new SstRematchApp(ctx, "sst_rematch",
+        PPCImageConfig));
+  }
+
+  // Override virtual hooks for customization:
+  // void OnPreSetup(rex::RuntimeConfig& config) override {}
+  // void OnPostSetup() override {}
+  // void OnCreateDialogs(rex::ui::ImGuiDrawer* drawer) override {}
+  // void OnShutdown() override {}
+  // void OnConfigurePaths(rex::PathConfig& paths) override {}
+};
